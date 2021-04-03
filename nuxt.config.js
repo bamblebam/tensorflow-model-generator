3e1fa232bf1c3bcb94be5d68c5360472b30a84b5
bamblebam <burhanizrangwala@gmail.com>
2021-04-03 21:31:34 +0530

    pause on persisted state

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,8 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    '~plugins/firebase.js'
+    '~plugins/firebase.js',
+    '~plugins/persistedState.client.js'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
