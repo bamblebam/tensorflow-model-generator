9331797f23af2b03da3a83eb05d8fdf903dc7d8e
therookie636 <therookie636@gmail.com>
2021-04-03 17:45:02 +0530

    Changed: button styles for sign in

--- components/SignInButton.vue
+++ components/SignInButton.vue
@@ -1,16 +1,18 @@
 <template>
-  <v-btn @click="signInPopup">Google Signin</v-btn>
+  <v-btn @click="signInPopup" color="#ff7000"
+    ><v-icon color="white">mdi-google</v-icon></v-btn
+  >
 </template>
 
 <script>
 export default {
   methods: {
-    signInPopup: async function () {
+    signInPopup: async function() {
       var provider = new this.$firebase.auth.GoogleAuthProvider();
       const result = await this.$firebase.auth().signInWithPopup(provider);
       var user = result.user;
       console.log(user);
-    },
-  },
+    }
+  }
 };
 </script>
