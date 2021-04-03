9a5f10301f57480a27abecb2949e158aadb4345d
bamblebam <burhanizrangwala@gmail.com>
2021-04-04 03:48:12 +0530

    compile

---
+++ backend_API/api.py
@@ -0,0 +1,13 @@
+import tensorflow as tf
+import tensorflow
+from tensorflow import keras
+from tensorflow.keras import layers
+
+code = """
+
+model = Sequential([
+Dense(units = 32, activation = 'relu'),
+Dense(units = 1),
+])
+
+"""
