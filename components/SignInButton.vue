d384315eefd5a5bd8cd829aab02c47cd64c9e4e4
bamblebam <burhanizrangwala@gmail.com>
2021-04-03 20:12:34 +0530

    global user start

--- components/SignInButton.vue
+++ components/SignInButton.vue
@@ -1,8 +1,6 @@
 <template>
   <v-btn @click="signInPopup" color="#ff7000">
-    <v-icon color="white">
-      mdi-google
-    </v-icon>
+    <v-icon color="white"> mdi-google </v-icon>
     <!-- if user logged in
     <v-icon color="white">
       mdi-exit-to-app
@@ -20,7 +18,7 @@ export default {
         .auth()
         .signInWithPopup(provider)
         .then((res) => {
-          user = result.user;
+          user = res.user;
           console.log(user);
         })
         .catch((error) => {
